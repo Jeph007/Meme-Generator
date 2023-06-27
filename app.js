c6fe7de4596b4083cc213c08f785a09e4a562feb
Jeph007 <133989489+Jeph007@users.noreply.github.com>
2023-06-27 17:52:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
+const memes = [];
+
 const createMeme = (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const imageUrl = document.getElementById('imageUrl').value;
-    const topText = document.getElementById('topText').value;
-    const bottomText = document.getElementById('bottomText').value;
+  const imageUrl = document.getElementById('imageUrl').value;
+  const topText = document.getElementById('topText').value;
+  const bottomText = document.getElementById('bottomText').value;
 
-    const memeContainer = document.getElementById('memeContainer');
-    memeContainer.innerHTML = '';
+  const memeContainer = document.getElementById('memeContainer');
 
-    const meme = document.createElement('div');
-    meme.className = 'meme-container';
+  const meme = document.createElement('div');
+  meme.className = 'meme-container';
 
-    const image = document.createElement('img');
-    image.src = imageUrl;
-    meme.appendChild(image);
+  const image = document.createElement('img');
+  image.src = imageUrl;
+  meme.appendChild(image);
 
-    const topTextDiv = document.createElement('div');
-    topTextDiv.className = 'meme-text meme-text-top';
-    topTextDiv.textContent = topText;
-    meme.appendChild(topTextDiv);
+  const topTextDiv = document.createElement('div');
+  topTextDiv.className = 'meme-text meme-text-top';
+  topTextDiv.textContent = topText;
+  meme.appendChild(topTextDiv);
 
-    const bottomTextDiv = document.createElement('div');
-    bottomTextDiv.className = 'meme-text meme-text-bottom';
-    bottomTextDiv.textContent = bottomText;
-    meme.appendChild(bottomTextDiv);
+  const bottomTextDiv = document.createElement('div');
+  bottomTextDiv.className = 'meme-text meme-text-bottom';
+  bottomTextDiv.textContent = bottomText;
+  meme.appendChild(bottomTextDiv);
 
-    memeContainer.appendChild(meme);
-};
+  memes.push(meme);
+  memeContainer.appendChild(meme);
+  meme.addEventListener('dblclick', function () {
+    this.remove();
+    memes.splice(memes.indexOf(meme), 1);
+  });
 
-const clearForm = () => {
-    document.getElementById('memeForm').reset();
-    document.getElementById('memeContainer').innerHTML = '';
+  document.getElementById('memeForm').reset();
 };
 
 const memeForm = document.getElementById('memeForm');
